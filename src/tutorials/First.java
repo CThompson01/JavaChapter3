2a67edddbc189caba3d71056b72f37a72dd86ba2
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-09-11 09:52:29 -0500

    Initial commit

---
+++ src/tutorials/First.java
@@ -0,0 +1,8 @@
+package tutorials;
+
+public class First {
+	public static void main(String[] args) {
+		Second.displayAddress();
+		System.out.println("First Java application");
+	}
+}
---
+++ src/youdoit/ParadiseInfo.java
@@ -0,0 +1,8 @@
+package youdoit;
+
+public class ParadiseInfo {
+	public static void displayInfo() {
+		System.out.println("Paradise Day Spa wants to pamper you.");
+		System.out.println("We will make you look good.");
+	}
+}
